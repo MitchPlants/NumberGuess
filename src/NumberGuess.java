0fc47ea5c42799ad47c3d86bf5f563ae2ec28aca
Mitch Plants <mitche..plants.2023@student.lhschools.org>
2019-10-25 09:45:14 -0400

    Game Loop

--- src/NumberGuess.java
+++ src/NumberGuess.java
@@ -1,24 +1,39 @@
+import java.util.Scanner;
+
 public class NumberGuess {
-    public static void main(String[]args){
+    public static void main(String[] args) {
+        int Guess,number;
+        String answer;
+        Scanner Keyboard;
+        Keyboard = new Scanner(System.in);
+        number = 5;
 
+            do{
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                Guess = Keyboard.nextInt();
+                if(number<Guess){
+                    System.out.println("Too high");
+                }
+                if (number>Guess) {
+                    System.out.println("Too Low");
+                }
+            }while(!(Guess == number));
 
 
     }
+    public static int generateGuess(int number) {
+        for(int i = 0; i<100; i++) {
+            number = (int) (Math.random()*100)+1;
 
+        }
+return number;
+
+    }
 }
+
+
+
+
+
+
+
